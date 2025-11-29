3dcf2f264cdb797587487131919ef6867314c6ac
ajrocks1 <138863009+ajrocks1@users.noreply.github.com>
2025-11-29 18:23:33 +0530

    Set up FastAPI with Uvicorn for the app

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from src.api_server import app
+import uvicorn
+from fastapi import FastAPI
+from src.api_server import router
+
+app = FastAPI(title="Bill Extractor Vision AI")
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
